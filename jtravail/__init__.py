f1d71c178fe8d8d6ae4f34d70854b87ad534bf3f
Jean-Loutre <jean-loutre@gmx.net>
2023-12-02 04:02:55 +0100

    refactor: move pomodoro state to it's own class

--- jtravail/__init__.py
+++ jtravail/__init__.py
@@ -1,15 +1,8 @@
-from datetime import datetime, timedelta
 from gettext import gettext as _
-from json import dumps as json_dumps
-from json import loads as json_loads
-from pathlib import Path
 
-from appdirs import user_cache_dir
 from click import echo, group
 
-_APP_NAME = "jean-travail"
-_APP_AUTHOR = "ottorg"
-_CACHE_DIR = Path(user_cache_dir(_APP_NAME, _APP_AUTHOR))
+from jtravail.pomodoro import Pomodoro, Status
 
 
 @group()
@@ -19,32 +12,24 @@ def cli() -> int:
 
 @cli.command()
 def start() -> int:
-    data = json_dumps({"started": datetime.now().isoformat()})
-    _CACHE_DIR.mkdir(parents=True, exist_ok=True)
-    with open(_CACHE_DIR / "current", "w") as cache_file:
-        cache_file.write(data)
-
+    pomodoro = Pomodoro()
+    pomodoro.start()
     echo(_("Pomodoro started"))
     return 0
 
 
 @cli.command()
 def status() -> int:
-    cache_file = _CACHE_DIR / "current"
-    if not cache_file.is_file():
+    pomodoro = Pomodoro()
+    (status, remaining) = pomodoro.status()
+    if status is Status.STOPPED:
         echo(_("stopped"))
     else:
-        with open(cache_file, "r") as cache:
-            data = json_loads(cache.read())
-            started = datetime.fromisoformat(data["started"])
-            elapsed = datetime.now() - started
-            remaining = timedelta(minutes=25) - elapsed
-            minutes = int(remaining.total_seconds() / 60)
-            seconds = abs(int(remaining.total_seconds() - 60 * minutes))
-            echo(
-                _("Pomodoro : {minutes}:{seconds}").format(
-                    minutes=minutes, seconds=seconds
-                )
-            )
+        assert remaining is not None
+        minutes = int(remaining.total_seconds() / 60)
+        seconds = abs(int(remaining.total_seconds() - 60 * minutes))
+        echo(
+            _("Pomodoro : {minutes}:{seconds}").format(minutes=minutes, seconds=seconds)
+        )
 
     return 0
---
+++ jtravail/pomodoro.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from enum import Enum
+from json import dumps as json_dumps
+from json import loads as json_loads
+from pathlib import Path
+
+from appdirs import user_cache_dir
+
+_APP_NAME = "jean-travail"
+_APP_AUTHOR = "ottorg"
+_CACHE_DIR = Path(user_cache_dir(_APP_NAME, _APP_AUTHOR))
+
+
+class Status(str, Enum):
+    STOPPED = "stopped"
+    POMODORO = "pomodoro"
+
+
+class Pomodoro:
+    def __init__(self) -> None:
+        self._state_path = _CACHE_DIR / "state"
+        self._status = Status.STOPPED
+        self._start_time: datetime | None = None
+        self.refresh()
+        pass
+
+    def start(self) -> None:
+        self._status = Status.POMODORO
+        self._start_time = datetime.now()
+        self._save()
+
+    def status(self) -> tuple[Status, timedelta | None]:
+        remaining = None
+        if self._start_time is not None:
+            elapsed = datetime.now() - self._start_time
+            remaining = timedelta(minutes=25) - elapsed
+        return self._status, remaining
+
+    def refresh(self) -> None:
+        if not self._state_path.is_file():
+            self._status = Status.STOPPED
+            self._start_time = None
+        else:
+            with self._state_path.open("r") as state_file:
+                data = json_loads(state_file.read() or "{}")
+                self._status = data.get("status", "stopped")
+                start_time_str = data.get("start_time", None)
+                self._start_time = start_time_str and datetime.fromisoformat(
+                    start_time_str
+                )
+
+    def _save(self) -> None:
+        data = {
+            "status": self._status,
+            "start_time": self._start_time and self._start_time.isoformat(),
+        }
+        self._state_path.parent.mkdir(parents=True, exist_ok=True)
+        with self._state_path.open("w") as state_file:
+            state_file.write(json_dumps(data))
